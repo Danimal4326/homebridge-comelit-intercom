d9e1f88c5a5d2e960df9a53e27621ceddf8278c5
Antonio Ibáñez <antoiba86@gmail.com>
2026-03-16 22:42:07 +0100

    Updating the project correctly

--- custom_components/comelit_intercom_local/const.py
+++ custom_components/comelit_intercom_local/const.py
@@ -1,6 +1,6 @@
 """Constants for the Comelit Local integration."""
 
-DOMAIN = "comelit_local"
+DOMAIN = "comelit_intercom_local"
 
 CONF_HTTP_PORT = "http_port"
 CONF_VIDEO_AUTO_RECONNECT = "video_auto_reconnect"
